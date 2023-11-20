469c5114c34d3503ef32828b39fd6e80c6b8ed8b
Araf Al Jami <clown1331@gmail.com>
2023-11-20 02:27:37 +0000

    Add docker

---
+++ sojj/HeartBeatBackgroundService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sojj
+{
+    public class HeartBeatBackgroundService : BackgroundService
+    {
+        private ILogger<HeartBeatBackgroundService> logger;
+
+        public HeartBeatBackgroundService(ILogger<HeartBeatBackgroundService> logger)
+        {
+            this.logger = logger;
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                logger.LogInformation("Heartbeat");
+                
+                
+                await Task.Delay(TimeSpan.FromSeconds(5), stoppingToken);
+            }
+        }
+    }
+}
