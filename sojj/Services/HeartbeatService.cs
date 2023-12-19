344710e59b894522496cca31a28c9c89d8843e69
Araf Al Jami <clown1331@gmail.com>
2023-12-19 04:14:18 +0000

    Heartbeat checkin

--- sojj/Services/HeartbeatService.cs
+++ sojj/Services/HeartbeatService.cs
@@ -7,12 +7,36 @@ public class HeartbeatService : BackgroundService
     private readonly ILogger<HeartbeatService> logger;
     private readonly IJudgeService judgeService;
     private readonly ISandboxService sandboxService;
+    private readonly IConfiguration configuration;
+    private readonly int heartbeatIntervalInSeconds;
+    private readonly TimeSpan heartbeatInterval;
+    private HttpClient? heartBeatCheckinClient = null;
 
-    public HeartbeatService(ILogger<HeartbeatService> logger, IJudgeService judgeService, ISandboxService sandboxService)
+    public HeartbeatService(ILogger<HeartbeatService> logger, IJudgeService judgeService, ISandboxService sandboxService, IConfiguration configuration)
     {
         this.logger = logger;
         this.judgeService = judgeService;
         this.sandboxService = sandboxService;
+        this.configuration = configuration;
+        this.heartbeatIntervalInSeconds = this.configuration.GetValue<int>("HeartbeatIntervalInSeconds");
+        this.heartbeatInterval = TimeSpan.FromSeconds(this.heartbeatIntervalInSeconds);
+        if (this.heartbeatIntervalInSeconds <= 0)
+        {
+            throw new ArgumentException("HeartbeatIntervalInSeconds must be greater than 0");
+        }
+
+        var heartbeatCheckinUrlString = this.configuration.GetValue<string>("HeartbeatCheckinUrl");
+
+        if (!string.IsNullOrWhiteSpace(heartbeatCheckinUrlString))
+        {
+            this.heartBeatCheckinClient = new HttpClient();
+            var heartbeatCheckinUrl = new Uri(heartbeatCheckinUrlString);
+            this.heartBeatCheckinClient.BaseAddress = heartbeatCheckinUrl;
+        }
+
+        logger.LogInformation("HeartbeatIntervalInSeconds: {heartbeatIntervalInSeconds}", heartbeatIntervalInSeconds);
+
+        logger.LogInformation("HeartbeatService started");
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -25,7 +49,28 @@ public class HeartbeatService : BackgroundService
 
             await sandboxService.CheckHealthAsync();
 
-            await Task.Delay(TimeSpan.FromMinutes(10), stoppingToken);
+            await CheckinAsync();
+
+            await Task.Delay(heartbeatInterval, stoppingToken);
+        }
+    }
+
+    private async Task CheckinAsync()
+    {
+        if (heartBeatCheckinClient is null)
+        {
+            return;
+        }
+        
+        var response = await heartBeatCheckinClient.GetAsync(string.Empty);
+
+        if (response.IsSuccessStatusCode)
+        {
+            logger.LogInformation("Heartbeat checkin success");
+        }
+        else
+        {
+            logger.LogError("Heartbeat checkin failed");
         }
     }
 }
