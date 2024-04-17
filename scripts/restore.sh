810559faa8ca8ca6be086f9f036074283ee00628
Araf Al Jami <11315571+ajami1331@users.noreply.github.com>
2024-04-17 16:13:47 +0100

    Update restore.sh

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -17,16 +17,16 @@ apt-get update && \
 
 BFLAT_VERSION=7.0.2
 
-wget https://github.com/bflattened/bflat/releases/download/v$BFLAT_VERSION/bflat-$BFLAT_VERSION-linux-glibc-x64.tar.gz
+wget https://github.com/bflattened/bflat/releases/download/v${BFLAT_VERSION}/bflat-${BFLAT_VERSION}-linux-glibc-x64.tar.gz
 
 rm -rf /usr/local/bflat
 
 mkdir -p /usr/local/bflat
 
-tar -xzf bflat-$BFLAT_VERSION-linux-glibc-x64.tar.gz -C /usr/local/bflat
+tar -xzf bflat-${BFLAT_VERSION}-linux-glibc-x64.tar.gz -C /usr/local/bflat
 
-rm -rf bflat-$BFLAT_VERSION-linux-glibc-x64.tar.gz
+rm -rf bflat-${BFLAT_VERSION}-linux-glibc-x64.tar.gz
 
 GO_JUDGE_VERSION=1.8.3
 
-wget https://github.com/criyle/go-judge/releases/download/v$GO_JUDGE_VERSION/go-judge_$GO_JUDGE_VERSION_linux_amd64 -O /usr/bin/sandbox && chmod +x /usr/bin/sandbox
+wget https://github.com/criyle/go-judge/releases/download/v${GO_JUDGE_VERSION}/go-judge_${GO_JUDGE_VERSION}_linux_amd64 -O /usr/bin/sandbox && chmod +x /usr/bin/sandbox
