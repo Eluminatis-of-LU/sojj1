88de5832013143838869076d9a80c14a3d7e887b
Araf Al Jami <11315571+ajami1331@users.noreply.github.com>
2024-04-17 15:57:33 +0100

    Update restore.sh

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -15,14 +15,18 @@ apt-get update && \
 		golang \
 		openjdk-8-jdk-headless
 
-wget https://github.com/bflattened/bflat/releases/download/v7.0.2/bflat-7.0.2-linux-glibc-x64.tar.gz
+BFLAT_VERSION=7.0.2
+
+wget https://github.com/bflattened/bflat/releases/download/v$(BFLAT_VERSION)/bflat-$(BFLAT_VERSION)-linux-glibc-x64.tar.gz
 
 rm -rf /usr/local/bflat
 
 mkdir -p /usr/local/bflat
 
-tar -xzf bflat-7.0.2-linux-glibc-x64.tar.gz -C /usr/local/bflat
+tar -xzf bflat-$(BFLAT_VERSION)-linux-glibc-x64.tar.gz -C /usr/local/bflat
 
-rm -rf bflat-7.0.2-linux-glibc-x64.tar.gz
+rm -rf bflat-$(BFLAT_VERSION)-linux-glibc-x64.tar.gz
 
-wget https://github.com/criyle/go-judge/releases/download/v1.8.1/go-judge_1.8.1_linux_amd64 -O /usr/bin/sandbox && chmod +x /usr/bin/sandbox
+GO_JUDGE_VERSION=1.8.3
+
+wget https://github.com/criyle/go-judge/releases/download/v$(GO_JUDGE_VERSION)/go-judge_$(GO_JUDGE_VERSION)_linux_amd64 -O /usr/bin/sandbox && chmod +x /usr/bin/sandbox
