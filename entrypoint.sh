c30c52cbd30bac2a22234f48ce2146e5efbf49ab
Araf Al Jami <clown1331@yahoo.com>
2024-05-09 20:02:46 +0100

    Add prefork

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
-/usr/bin/sandbox -http-addr 0.0.0.0:5050 -dir ~/sandbox/ -release -file-timeout 30m &
+#!/bin/bash
+
+if [ -z "$PREFORK" ]; then
+  PREFORK=1
+fi
+
+/usr/bin/sandbox -http-addr 0.0.0.0:5050 -dir ~/sandbox/ -release -file-timeout 30m -pre-fork=$PREFORK &
 
 dotnet /app/Sojj.dll
