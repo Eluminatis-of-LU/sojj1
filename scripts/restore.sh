6654dfbac689c17c57e1e72bca7a104ffc5b5f8d
Araf Al Jami <clown1331@yahoo.com>
2024-05-09 20:45:11 +0100

    Manually install kotlinc

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -16,9 +16,7 @@ apt-get update && \
 		ruby \
 		golang \
 		openjdk-8-jdk-headless \
-        curl \
-		unzip \
-        zip
+		unzip
 
 BFLAT_VERSION=7.0.2
 
@@ -32,12 +30,18 @@ tar -xzf bflat-${BFLAT_VERSION}-linux-glibc-x64.tar.gz -C /usr/local/bflat
 
 rm -rf bflat-${BFLAT_VERSION}-linux-glibc-x64.tar.gz
 
-curl -s "https://get.sdkman.io" | bash
-
-source "/root/.sdkman/bin/sdkman-init.sh"
-
-sdk install kotlin
-
 GO_JUDGE_VERSION=1.8.4
 
 wget https://github.com/criyle/go-judge/releases/download/v${GO_JUDGE_VERSION}/go-judge_${GO_JUDGE_VERSION}_linux_amd64 -O /usr/bin/sandbox && chmod +x /usr/bin/sandbox
+
+KOTLIN_VERSION=1.9.24
+
+wget https://github.com/JetBrains/kotlin/releases/download/v${KOTLIN_VERSION}/kotlin-compiler-${KOTLIN_VERSION}.zip
+
+unzip kotlin-compiler-${KOTLIN_VERSION}.zip -d ./tmp_kotlinc
+
+cp -r ./tmp_kotlinc/kotlinc/bin /usr/local/
+cp -r ./tmp_kotlinc/kotlinc/lib /usr/local/
+
+rm -rf ./tmp_kotlinc
+rm -rf kotlin-compiler-${KOTLIN_VERSION}.zip
