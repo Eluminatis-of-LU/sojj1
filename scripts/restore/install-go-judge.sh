047f85c06d94fb8e0bd8774d82185ec233d7b430
Araf Al Jami <11315571+ajami1331@users.noreply.github.com>
2024-05-24 02:18:33 +0100

    Update install-go-judge.sh (#26)

--- scripts/restore/install-go-judge.sh
+++ scripts/restore/install-go-judge.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-GO_JUDGE_VERSION=1.8.4
+GO_JUDGE_VERSION=1.8.5
 
 wget https://github.com/criyle/go-judge/releases/download/v${GO_JUDGE_VERSION}/go-judge_${GO_JUDGE_VERSION}_linux_amd64 -O /usr/bin/sandbox && chmod +x /usr/bin/sandbox
